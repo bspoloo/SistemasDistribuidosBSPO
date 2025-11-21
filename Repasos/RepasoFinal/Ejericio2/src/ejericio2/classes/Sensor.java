70406a065262310305e029269097eb6282360e50
bspoloo <animetx098@gmail.com>
2025-11-21 16:32:23 -0400

    added parcial final

---
+++ Repasos/RepasoFinal/Ejericio2/src/ejericio2/classes/Sensor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejericio2.classes;
+
+import ejericio2.enums.Status;
+
+/**
+ *
+ * @author Animetx
+ */
+public class Sensor {
+    private String id;
+    private Status status; 
+
+    public Sensor(String id, Status status) {
+        this.id = id;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    
+    
+}
